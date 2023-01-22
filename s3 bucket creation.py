63d11ce5a67fc8de0dee51ab57a11c162313e607
Udit <emfreak22@gmail.com>
2023-01-22 11:13:02 -0800

    Added s3 bucket file insertion code, with automatic session and config

--- s3 bucket creation.py
+++ s3 bucket creation.py
@@ -1,19 +1,42 @@
+import os
 import boto3
-s3connection = boto3.resource("s3")
-def bucketCreation():
-    bucket = s3connection.Bucket("emfreak22s3bucket")
-    response = bucket.create(
-        ACL = 'public-read',
-        CreateBucketConfiguration = {"LocationConstraint" : 'eu-central-1'}
-    )
+from configparser import ConfigParser
+configur = ConfigParser()
+print(configur.read('config/main_config.ini'))
+print(os.getcwd())
 
-    print(response)
+class s3Ops:
+    def __init__(self):
+        self.session = self.createSession()
+        self.s3connection = self.session.resource("s3")
+    def createSession(self):
+        id = configur.get('AWS','id')
+        secret =configur.get('AWS','secret')
+        session = boto3.Session(id,secret)
+        return session
 
-def listAllBuckets():
-    allBuckets = s3connection.buckets.all()
-    print(list(allBuckets))
+    def bucketCreation(self, bucketName):
+        bucket = self.s3connection.Bucket(bucketName)
+        response = bucket.create(
+            ACL = 'public-read',
+            CreateBucketConfiguration = {"LocationConstraint" : 'eu-central-1'}
+        )
+        print(response)
 
+    def listAllBuckets(self):
+        allBuckets = self.s3connection.buckets.all()
+        print(list(allBuckets))
+
+    def insertIntoBucket(self, bucketName,file, text):
+        fileObj = self.s3connection.Object(bucketName,text)
+        fileObj.put(Body=open(file, 'rb'))
 
 if __name__ == '__main__':
     # bucketCreation()
-    listAllBuckets()
+    # listAllBuckets()
+    obj = s3Ops()
+    # obj.bucketCreation('bucketsss091')
+    # file  = 'C:\\Users\\Udit\\Downloads\\TereNaam2003SalmanKhan_archive.torrent'
+    # text = 'tere_naam.torrent'
+    # obj.insertIntoBucket('bucketsss091',file,text)
+    
